a11cfdbb2b1a50d71b9c8963da17ef8775c760dd
RampagingHippy <sahkounts@gmail.com>
2014-06-20 06:58:40 -0400

    renamed to PlayerControls for clarity, updated to include attack key

--- DoodleWarz/Structs + Enums/PlayerControls.cs
+++ DoodleWarz/Structs + Enums/PlayerControls.cs
@@ -10,12 +10,13 @@ namespace DoodleWarz
     //All the actions a player may take
     //each tied to a key or gamepad button
     //for use by players for reading inputs
-    public struct PlayerActionStruct
+    public struct PlayerControls
     {
         public Keys moveUp;
         public Keys moveDown;
         public Keys moveLeft;
         public Keys moveRight;
         public Keys pause;
+        public Keys attack;
     }
 }
